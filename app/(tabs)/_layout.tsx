f1a245fa4202c5e9b8496e6d9c7703a670fcc7c4
sam_nolimit <tadejolu@gmail.com>
2025-07-22 20:55:56 +0100

    project set-up and tablayout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -20,7 +20,6 @@ export default function TabLayout() {
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
-            // Use a transparent background on iOS to show the blur effect
             position: 'absolute',
           },
           default: {},
@@ -34,9 +33,16 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="addExpense"
         options={{
           title: 'Explore',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="analytics"
+        options={{
+          title: 'Analytics',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
---
+++ app/(tabs)/analytics.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const AnalyticsScreen = () => {
+  return (
+    <View>
+      <Text>analytics</Text>
+    </View>
+  );
+};
+
+export default AnalyticsScreen;
+
+const styles = StyleSheet.create({});
